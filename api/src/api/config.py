90a46f9bdcb41017c29463f2e6b6c0b44ec0b3e5
alexgara <alexgara.git@simonides.anonaddy.com>
2026-03-04 17:57:29 +0100

    Add cognito hostname

--- api/src/api/config.py
+++ api/src/api/config.py
@@ -5,7 +5,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     device: str = "cuda" if torch.cuda.is_available() else "cpu"
     hf_cache_dir: str = "hub_cache"
-    cors_origins: list[str] = ["http://localhost:5173"]
+    cors_origins: list[str] = [
+        "http://localhost:5173",
+        "http://cognito",
+        "http://cognito:5173",
+    ]
     default_beam_width: int = 5
     max_input_length: int = 500
 
